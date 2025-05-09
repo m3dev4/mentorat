e591e03ef03e74f179f811e470250adb32dc9ecb
m3dev4 <m3dev4@gmail.com>
2025-05-09 16:47:37 +0000

    feat: add new components and update dependencies for frontend layout
    - Added new layout components: Hero, Cta, Instructors, Footer, Category, Testimony, NavBar, and Button
    - Updated package.json with new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, and tw-animate-css
    - Modified welcome.tsx to integrate new components and enhance layout structure
    - Updated app.css with custom variants and utility classes for styling

---
+++ frontend/app/components/layouts/cta.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Category from "./category";
+
+const Cta = () => {
+  return (
+    <div>
+      Cta
+      <Category />
+    </div>
+  );
+};
+
+export default Cta;
--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/layouts/navBar";
+import Hero from "../components/layouts/hero";
+import Cta from "../components/layouts/cta";
+import Instructors from "../components/layouts/instructors";
+import Footer from "../components/layouts/footer";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
-  )
-}
+    <main className="w-full h-full ">
+        <NavBar />
+        <div className="flex flex-col items-center justify-center m-auto p-auto overflow-auto h-full w-full max-w-7xl">
+        <Hero />
+        <Cta />
+        <Instructors />
+        <Footer />
+      </div>
+    </main>
+  );
+};
 
-export default Welcome
+export default Welcome;
